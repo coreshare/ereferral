f98226067bf6df9f5d2d095c7d528ee13f124f73
Coreshare <devops@coreshare.co.uk>
2023-07-30 18:08:19 +0530

    change 4

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import React, {useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  useEffect(()=>{
+    fetchApi();
+  },[]);
+  const fetchApi = () =>{
+    fetch("https://ereferralapi.azurewebsites.net/WeatherForecast")
+    .then(res => res.json())
+            .then(
+                (data) => {
+                    debugger;
+                },
+                (error) => {
+                  debugger;
+                }
+            )
+  }
+
   return (
     <div className="App">
       <header className="App-header">
