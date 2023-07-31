f7883f325e556e8adba91010b98e912f13235a5e
Coreshare <devops@coreshare.co.uk>
2023-07-31 08:20:50 +0530

    change 6

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to e-Referral Programme.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
       </header>
     </div>
