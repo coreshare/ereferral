a69a718925d0906743a7158c9f15ffe88d3f12a0
Coreshare <devops@coreshare.co.uk>
2023-08-11 19:07:56 +0530

    app changes 7

--- src/components/Parent/Parent.jsx
+++ src/components/Parent/Parent.jsx
@@ -25,7 +25,7 @@ const Parent = () => {
     var itemId = await saveData(formData);
     console.log(itemId);
     for(var i=0;i < attachments.length;i++){
-        attachmentsMetadata[attachments[i].name].DataSetID=itemId.ToString();
+        attachmentsMetadata[attachments[i].name].DataSetID=itemId;
         console.log(attachmentsMetadata[attachments[i].name]);
         console.log(JSON.stringify(attachmentsMetadata[attachments[i].name]));
         uploadFileToLib(attachments[i],attachmentsMetadata[attachments[i].name]);
--- src/services/api.js
+++ src/services/api.js
@@ -34,7 +34,7 @@ export const saveData = async (data) => {debugger;
       throw new Error("Failed to submit data");
     }
     const responseBody = await response.json();
-    return responseBody;
+    return responseBodytoString();
   } catch (error) {
     throw new Error("Failed to submit data");
   }
