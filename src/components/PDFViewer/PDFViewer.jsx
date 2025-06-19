0202d2c61c81a190cc4c771eb1aceda9992fec03
clatterbridgecancercentre <eReferrals@ClatterbridgeCC.onmicrosoft.com>
2025-06-19 12:05:19 +0530

    pdf changes 01

--- src/components/PDFViewer/PDFViewer.jsx
+++ src/components/PDFViewer/PDFViewer.jsx
@@ -1,37 +1,32 @@
 import React, { useState, useEffect } from "react";
-import {Viewer, Worker} from "@react-pdf-viewer/core"
-import { DefaultLayoutPlugin, defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
-import '@react-pdf-viewer/default-layout/lib/styles/index.css'
+import { Viewer, Worker } from "@react-pdf-viewer/core";
+import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
+import "@react-pdf-viewer/default-layout/lib/styles/index.css";
+import "@react-pdf-viewer/core/lib/styles/index.css";
+
+const workerUrl = new URL("pdfjs-dist/build/pdf.worker.min.js", import.meta.url);
 
 const PDFViewer = ({ file }) => {
     const [pdfFile, setPDFFile] = useState(null);
     const [viewPDF, setViewPDF] = useState(null);
 
     useEffect(() => {
-        let reader = new FileReader();
+        const reader = new FileReader();
         reader.readAsDataURL(file);
-        reader.onload = (e) => {
-            setPDFFile(e.target.result);
-        };
+        reader.onload = (e) => setPDFFile(e.target.result);
     }, [file]);
 
     useEffect(() => {
-        if (pdfFile !== null) {
-            setViewPDF(pdfFile);
-        }
+        if (pdfFile) setViewPDF(pdfFile);
     }, [pdfFile]);
 
     const newPlugin = defaultLayoutPlugin();
 
     return (
-        <div>
-            <div className="pdf-container">
-                <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
-                    {viewPDF && (
-                        <Viewer fileUrl={viewPDF} plugins={[newPlugin]} />
-                    )}
-                </Worker>
-            </div>
+        <div className="pdf-container">
+            <Worker workerUrl={workerUrl.toString()}>
+                {viewPDF && <Viewer fileUrl={viewPDF} plugins={[newPlugin]} />}
+            </Worker>
         </div>
     );
 };
