55ca713bbc83f90bbd76b07187eb8deb8b2d42fc
clatterbridgecancercentre <eReferrals@ClatterbridgeCC.onmicrosoft.com>
2026-01-29 12:58:09 +0530

    app changes 1

--- src/components/UserValidation/UserValidation.jsx
+++ src/components/UserValidation/UserValidation.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import EmailOTPRequest from '../EmailOTPRequest/EmailOTPRequest';
 import OTPValidation from '../OTPValidation/OTPValidation';
 import ClatterbridgeLogo from '../../Images/Clatterbridge-logo.png';
 import "./UserValidation.css";
 import { useSelector } from "react-redux";
+import { getMasterData } from "../../Services/api";
 
 const UserValidation = () => {
     const currentStep = useSelector(state => state.userValidationStep)
+    const [appOfflineFlag,setAppOfflineFlag] = useState();
+
     /*const [currentStep, setCurrentStep] = useState(0);
     const [otp, setOTP] = useState("");
 
@@ -21,7 +24,21 @@ const UserValidation = () => {
     const goToReferralSelection = () => {
         onNext();
     }*/
-    
+    useEffect(() => {
+        getMasterData("Configurations")
+              .then((data) => {
+                const offlineConfig = data.find(
+                    item => item.Title === "App Offline Flag"
+                )
+
+                // Read value safely
+                const isOffline =
+                    offlineConfig?.Value?.toLowerCase() === "yes"
+
+                setAppOfflineFlag(isOffline)
+              })
+    },[])
+
     return(
         <div class="container">
             <div class="vertical-center">
@@ -33,8 +50,20 @@ const UserValidation = () => {
                 <p class="boldtxt" style={{ lineHeight: '50px', fontSize: '30px', marginTop: '0px' }}>
                     Patient Referral Portal
                 </p>
-                {currentStep === 0 && <EmailOTPRequest />}
-                {currentStep === 1 && <OTPValidation />}
+                {appOfflineFlag ? (
+                    <>
+                        <p class="headline">We'll be back</p>
+                        <p class="message">We're busy updating the app for you</p>
+                        <p class="message">Please check back later today</p>
+                    </>
+                ) : (
+                    <>
+                        {currentStep === 0 && <EmailOTPRequest />}
+                        {currentStep === 1 && <OTPValidation />}
+                    </>
+                )}
+                {/*{currentStep === 0 && <EmailOTPRequest />}
+                {currentStep === 1 && <OTPValidation />}*/}
             </div>
         </div>
     )
