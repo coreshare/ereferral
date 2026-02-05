30d70e17990acf6fefb7701ef1b8206223f7543d
clatterbridgecancercentre <eReferrals@ClatterbridgeCC.onmicrosoft.com>
2026-02-05 10:30:16 +0530

    submit button disable 5

--- src/components/SubmitReferral/SubmitReferral.jsx
+++ src/components/SubmitReferral/SubmitReferral.jsx
@@ -113,7 +113,7 @@ const SubmitReferral = () => {
 
     return(
         <div className="container-submit">
-            <div style={{display: 'inline-block',width:'100%'}}>{isSubmitting} && "test1" : "test2"
+            <div style={{display: 'inline-block',width:'100%'}}>{isSubmitting ? "test1" : "test2"}
                 <h3 className="detailsHeader" style={{float:'left'}}>Submit Referral</h3>
                 <button onClick={handleBack} className="buttonCtrl" style={{float:'right'}}>Back</button>
             </div>
